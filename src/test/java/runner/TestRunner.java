678d6386817bfce43c359253d19bd231f765d812
yasin.aroglan <yaroglan@ford.com.tr>
2025-02-18 21:26:17 +0300

    commit end

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,18 +1,22 @@
 package runner;
 
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
+
 @CucumberOptions(
         features = "src/test/java/features",
         glue = {"utils", "stepDefinition"},
         plugin = {"pretty", "html:target/cucumber-html-report", "json:cucumber.json"}
         )
 
-public class TestRunner{
+public class TestRunner extends AbstractTestNGCucumberTests{
 
-
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
